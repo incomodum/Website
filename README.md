65e3755b1a5402df0f6269d09f010533a45d2c6b
Mindstormjak <mindstormjak@gmail.com>
2024-09-17 09:02:50 +0200

    git: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Incomodum Web
---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#1C274C",
+				primary2: "#FF6F61",
+				foreground: "#2C3A4B"
+			},
+			boxShadow: {
+				button: "10px 12px 45px 7px color-mix(in srgb, currentColor 50%, transparent)"
+			},
+			fontFamily: {
+				sans: ['DM Sans Variable', ...defaultTheme.fontFamily.sans]
+			}
+		},
+	},
+	plugins: [],
+}
